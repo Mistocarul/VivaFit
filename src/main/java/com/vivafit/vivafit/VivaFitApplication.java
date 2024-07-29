315eabe3622eeb3ca840021e8000b87fe4c69fcb
Mistocarul <pauleusebiubejan@gmail.com>
2024-07-29 15:26:23 +0300

    Inceputul

---
+++ src/main/java/com/vivafit/vivafit/VivaFitApplication.java
@@ -0,0 +1,13 @@
+package com.vivafit.vivafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivafit/vivafit/VivaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivafit.vivafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
